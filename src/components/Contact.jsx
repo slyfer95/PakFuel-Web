1e543c8214b48015066d63a43b4f4edb65095154
Waleed Ahmed <waleedporsche@gmail.com>
2024-06-25 23:43:57 +0500

    Contact Info Updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,8 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact Us</h1>
-      <p>Email: contact@pakfuel.com</p>
-      <p>Phone: +123 456 7890</p>
+      <p>Email: waleedbarozai@gmail.com</p>
+      <p>Phone: +923168296885</p>
     </div>
   );
 };
